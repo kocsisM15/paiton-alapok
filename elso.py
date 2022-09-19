2fb1f03078b17fbf22e17af295482246fbebbea6
kocsisM15 <buzivagyok123a@gmail.com>
2022-09-19 10:11:29 +0200

    harmadik commit

--- elso.py
+++ elso.py
@@ -2,11 +2,3 @@ from os import system
 
 system("cls")
 
-
-elsoValtozo = 12
-
-print(elsoValtozo)
-
-elsoValtozo = "A 10 a a legjobb"
-
-print(elsoValtozo)
---
+++ tallon_1.py
@@ -0,0 +1,19 @@
+#int - típus
+elsoValtozo = 12
+print(type(elsoValtozo))
+
+#str - típus
+elsoValtozo = "változó"
+print(type(elsoValtozo))
+
+#float - típus
+elsoValtozo = 12.35
+print(type(elsoValtozo))
+
+#bool - típus
+elsoValtozo  = True
+print(type(elsoValtozo))
+
+#list - típus
+elsoValtozo  = [1, 2]
+print(type(elsoValtozo))
